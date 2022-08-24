4d63af64135a35d77050f122c25c84f4ba7b002d
nyawira4400 <brenda.kinyua@student.moringaschool.com>
2022-08-24 16:35:58 +0300

    Completed assignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -8,7 +8,7 @@ describe('index.js', function () {
     it('is set as Scuber', function () {
       expect(companyName).to.equal('Scuber');
     });
-
+    
     it('is defined as a const', function () {
       expect(js).to.match(/const companyName/, "Expected companyName to be a const");
     });
